653276d0dc197db84888cce70c42df4654d9a7d5
justsimple <stevealane.damennjateng.ictuniversity.edu.cm>
2024-12-13 23:33:53 +0100

    feat: changed users class to be an abstract class and modified camdidate to inherin from it

--- src/classes/candidates.java
+++ src/classes/candidates.java
@@ -1,50 +1,51 @@
 package classes;
-import java.util.HashMap;
-public class candidates {
- private  int  candidate_id;
- private String candidate_name;
+
+
+public class candidates extends users {
  private String party;
-private   HashMap<String, Object>boigraphy=new HashMap<>();
- private int count=0;
+ private int vote_count=0;
+public  String nationality;
+public  String  short_notes;
 
-
-    public candidates(int candidate_id,String cantidate_name,String party,int count){
-        this.candidate_id=candidate_id;
-        this.candidate_name=cantidate_name;
+    public candidates(int user_id, String user_name, String user_password,String user_email,
+    int date_of_birth,String place_of_birth,String party,int vote_count,String nationality,String short_notes){
+      super(user_id, user_name, user_password, user_email,date_of_birth,place_of_birth);
+   
         this.party=party;
-        this.count=count;
+        this.vote_count=vote_count;
+        this.nationality=nationality;
+        this.short_notes=short_notes;
     }
      // getters methods
 
-
-    public int getCandidate_id() {
-        return candidate_id;
-    }
-
-    public String getCandidate_name() {
-        return candidate_name;
-    }
+    
     public String getparty(){
         return party;
     }
 
-    public int getCount(){
-        return count;
+    public int getvote_vote_count(){
+        return vote_count;
+    }
+    public String get_nationality(){
+        return nationality;
+    }
+    public String get_shortnotes(){
+        return short_notes;
     }
     // setters methods
-    public void setCandidate_id(String candidate_name){
-        this.candidate_name=candidate_name;
-    }
 
-    public void setCandidate_id(int candidate_id) {
-        this.candidate_id = candidate_id;
-    }
 
     public void setParty(String party) {
         this.party = party;
     }
 
-    public void setCount(int count) {
-
+    public void setvote_count(int vote_vote_count) {
+        this.vote_count=vote_vote_count;
+    }
+    public void set_nationality(String nationality){
+        this.nationality=nationality;
+    }
+    public void set_short_notes(String short_notes){
+        this.short_notes=short_notes;
     }
 }
--- src/classes/users.java
+++ src/classes/users.java
@@ -1,19 +1,23 @@
 package classes;
 
-public class users {
+@SuppressWarnings({"InitializerMayBeStatic", "UnnecessaryReturnStatement"})
+public abstract  class users {
     private  int user_id;
     private String user_name;
     private  String user_password;
     private final String user_email;
-
-        public users(int user_id, String user_name, String user_password,String user_email) {
+    private int  date_of_birth;
+    private String  place_of_birth;
+        public users(int user_id, String user_name, String user_password,String user_email,int date_of_birth,String place_of_birth) {
             this.user_id = user_id;
             this.user_name = user_name;
             this.user_password = user_password;
             this.user_email=user_email;
+            this.date_of_birth=date_of_birth;
+            this.place_of_birth=place_of_birth;
         }
 
-
+//getters methods
     public int getUser_id() {
         return user_id;
     }
@@ -29,7 +33,13 @@ public class users {
     public String getUser_email() {
         return user_email;
     }
-
+    public int get_birth_date(){
+         return date_of_birth;
+         }
+   public String get_birth_place(){
+         return place_of_birth;
+         }
+     //setters methods
     public void  setUser_id(int user_id) {
 
         this.user_id = user_id;
@@ -46,6 +56,13 @@ public class users {
 
     public void setUser_email(String user_email) {
 
+    }
+    public void setdate_of_birt(int birth_date){
+        this.date_of_birth=birth_date;
+    }
+    
+    public void set_place_of_birth(String birth_place){
+        this.place_of_birth=birth_place;
     }
 
     public static void vote(){
