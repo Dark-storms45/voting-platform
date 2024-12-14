68aaba1883b15637aa14ed26132ba7d084eabf49
justsimple <stevealane.damennjateng.ictuniversity.edu.cm>
2024-12-14 23:07:29 +0100

    done

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import utility.Display;
 public class App {
     public static void main(String[] args) {
+
       System.out.println("Hello World");
       Display.displayBox("hello everyone");
 
--- src/utility/Display.java
+++ src/utility/Display.java
@@ -6,7 +6,7 @@ public class Display {
         int textLength = text.length(); // Get the length of the text
         int paddingSize = 4; // Set padding size
         int boxWidth = textLength + paddingSize * 2; // Calculate the width of the box
-
+         
         // Print the top border
         for (int i = 0; i < boxWidth; i++) {
             System.out.print("-");
