12628a58976811414f374e063b10fe219c4916c0
justsimple <stevealane.damennjateng.ictuniversity.edu.cm>
2024-12-18 10:38:31 +0100

    created a poll class

---
+++ src/classes/poll.java
@@ -0,0 +1,10 @@
+package classes;
+import java.util.List;
+
+public class Poll {
+    private List<candidates> candidate ;
+    public Poll(List<candidates> candidate){
+        this.candidate=candidate;
+    }
+
+}
--- src/classes/user.java
+++ src/classes/user.java
@@ -1,5 +1,13 @@
 package classes;
 
-public class user {
+    @SuppressWarnings({"InitializerMayBeStatic", "UnnecessaryReturnStatement"})
+    class User extends users{
+    
+    public User(int user_id,String user_name,String user_password,String user_email,int date_of_birth,String place_of_birth){
+        super(user_id, user_name, user_password, user_email, date_of_birth, place_of_birth);
+    
+    }
+    
+    
+    }
 
-}
